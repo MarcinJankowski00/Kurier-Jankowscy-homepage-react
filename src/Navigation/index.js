49dc7f5ce1eaaeb9466bab2fc62c784131938c4e
Marcin Jankowski <marcin5771@gmail.com>
2023-07-13 15:09:31 +0200

    fixed navigation

--- src/Navigation/index.js
+++ src/Navigation/index.js
@@ -1,24 +1,41 @@
 import { List, Link, Nav } from "./styled";
-const Navigation = () => (
-    <Nav>
-        <List>
-            <li>
-                <Link href="#Aktualnosci">Aktualności</Link>
-            </li>
-            <li>
-                <Link href="#Onas">O nas</Link>
-            </li>
-            <li>
-                <Link href="#Oferta">Oferta</Link>
-            </li>
-            <li>
-                <Link href="#Rozklad">Rozkład jazdy</Link>
-            </li>
-            <li>
-                <Link href="#Kontakt">Kontakt</Link>
-            </li>
-        </List>
-    </Nav>
-)
+const Navigation = () => {
+    
+    const handleLinkClick = (event, targetId) => {
+        event.preventDefault();
+        const targetElement = document.getElementById(targetId);
+        const navigationHeight = 170;
+        const topPosition = targetElement.offsetTop - navigationHeight;
+        window.scrollTo({
+          top: topPosition,
+          behavior: 'smooth',
+        });
+      };
+
+    return (
+        <Nav>
+            <List>
+                <li>
+                    <Link href="#Aktualnosci" onClick={(event) => handleLinkClick(event, 'Aktualnosci')}>Aktualności</Link>
+                </li>
+                <li>
+                    <Link href="#Onas" onClick={(event) => handleLinkClick(event, 'Onas')}>O nas</Link>
+                </li>
+                <li>
+                    <Link href="#Oferta" onClick={(event) => handleLinkClick(event, 'Oferta')}>Oferta</Link>
+                </li>
+                <li>
+                    <Link href="#Rozklad" onClick={(event) => handleLinkClick(event, 'Rozklad')}>Rozkład jazdy</Link>
+                </li>
+                <li>
+                    <Link href="#Flota" onClick={(event) => handleLinkClick(event, 'Flota')}>Flota</Link>
+                </li>
+                <li>
+                    <Link href="#Kontakt" onClick={(event) => handleLinkClick(event, 'Kontakt')}>Kontakt</Link>
+                </li>
+            </List>
+        </Nav>
+    );
+};
 
 export default Navigation;

72d589a36c29c912539a20840ea0df2f526fe151
Marcin Jankowski <marcin5771@gmail.com>
2023-08-18 22:50:20 +0200

    content updated

--- src/Content/Aktualnosci/index.js
+++ src/Content/Aktualnosci/index.js
@@ -6,7 +6,7 @@ const Aktualnosci = () => (
         <Container>
         <Span>DRODZY PASAŻEROWIE</Span><br />
         Informujemy, że w dniu 18.08.2023 r. (piątek) została zmieniona lokalizacja przystanku znajdującego
-        się przy Dworzecu PKS w związku z trwającą budową. Miejsce postojowe zostanie przeniesione w miejsce
+        się przy Dworzecu PKS w związku z trwającą budową. Miejsce postojowe zostało przeniesione w miejsce
         oznaczone na załączonym planie sytuacyjnym.
         </Container>
         <Image src={info} />

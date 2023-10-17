984b438adc467dc9f962f173f580f952645df495
Marcin Jankowski <marcin5771@gmail.com>
2023-10-17 12:45:52 +0200

    smal fix

--- src/Content/Aktualnosci/index.js
+++ src/Content/Aktualnosci/index.js
@@ -10,7 +10,7 @@ const Aktualnosci = () => (
                 DRODZY PASAŻEROWIE
             </Div><br />
             Informujemy, że w dniu 16.10.2023 r. (poniedziałek) została zmieniona lokalizacja przystanku znajdującego
-            się przy Dworzecu PKS. Miejsce postojowe zostało przeniesione na stałe, w miejsce
+            się przy Dworcu PKS. Miejsce postojowe zostało przeniesione na stałe, w miejsce
             oznaczone na załączonym planie sytuacyjnym.
         </Container>
         <Image src={info} />

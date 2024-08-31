68c42297d6d64281ca34017cac87a1da87cfb9a4
Marcin Jankowski <marcin5771@gmail.com>
2024-08-31 20:28:36 +0200

    visual changes

--- src/Content/Aktualnosci/styled.js
+++ src/Content/Aktualnosci/styled.js
@@ -3,11 +3,11 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     display: grid;
-    grid-template-columns: 1fr auto;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 30px 100px;
     @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
+        grid-template-columns: 1fr;
+        grid-gap: 10px 100px;
     }
 `;
 
@@ -17,32 +17,35 @@ export const Content = styled(Link)`
     display: flex;
     justify-self: start;
     align-items: center;
-    max-width: 500px;
     margin: 30px 0;
-    padding-right: 12px;
     transition: 0.5s;
+    border-radius: 4px;
     &:hover {
         background-color: ${({ theme }) => theme.color.white};
+        box-shadow: 0px 4px 12px 0px rgba(186, 199, 213, 0.5);
     }
     @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax21}px) {
         margin: 15px 0;
+        background-color: ${({ theme }) => theme.color.white};
+        box-shadow: 0px 4px 12px 0px rgba(186, 199, 213, 0.5);
     }
 `;
 
 export const Photo = styled.img`
     width: 50%;
-    margin-right: 20px;
-    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax22}px) {
+    margin: 16px;
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
         width: 35%;
-        margin-right: 10px;
     }
 `;
 
 export const Info = styled.div`
     width: 50%;
-    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax22}px) {
+    margin: 16px 16px 16px 0;
+    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
         width: 65%;
     }
+
 `;
 
 export const Title = styled.div`
@@ -59,9 +62,6 @@ export const FirstSentence = styled.div`
     color: ${({ theme }) => theme.color.lessImportant};
     @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax21}px) {
         font-size: 12px;
-    }
-    @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax22}px) {
-        display: none;
     }
 `;
 

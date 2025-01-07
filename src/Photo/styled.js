a0d0149c661ab30323d2beb6af512e807ec0c0c6
Marcin Jankowski <marcin5771@gmail.com>
2025-01-07 11:52:51 +0100

    deleted christmas mode

--- src/Photo/styled.js
+++ src/Photo/styled.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import background from "./background.png";
+import background from "./background.jpg";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 
 
